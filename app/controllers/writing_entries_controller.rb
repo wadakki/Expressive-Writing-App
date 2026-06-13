31e1dd6bf39101276f39dec510d1de1370cc2d0d
Tomoyuki <tomoyuki@DESKTOP-V1L3SK5.localdomain>
2026-06-13 19:37:07 +0900

    issue#12の筆記開示保存処理を追加しました。

--- app/controllers/writing_entries_controller.rb
+++ app/controllers/writing_entries_controller.rb
@@ -4,4 +4,29 @@ class WritingEntriesController < ApplicationController
   def new
     @writing_entry = current_user.writing_entries.build
   end
+
+  def create
+    @writing_entry = current_user.writing_entries.build(writing_entry_params)
+
+    if @writing_entry.save
+      redirect_to root_path, notice: t(".#{@writing_entry.status}_success")
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def writing_entry_params
+    params.require(:writing_entry).permit(
+      :before_happiness_score,
+      :after_happiness_score,
+      :event_detail,
+      :negative_emotion_detail,
+      :positive_emotion_detail,
+      :unforgiven_target_detail,
+      :tomorrow_hope,
+      :status
+    )
+  end
 end
--- test/controllers/writing_entries_controller_test.rb
+++ test/controllers/writing_entries_controller_test.rb
@@ -37,7 +37,8 @@ class WritingEntriesControllerTest < ActionDispatch::IntegrationTest
                     "writing_entry[#{attribute}]"
     end
 
-    assert_select "input[type=submit][value=?]", "投稿する"
+    assert_select "button[name=?][value=draft]", "writing_entry[status]", "下書き保存"
+    assert_select "button[name=?][value=completed]", "writing_entry[status]", "投稿する"
   end
 
   test "shows all fixed questions" do
@@ -60,6 +61,79 @@ class WritingEntriesControllerTest < ActionDispatch::IntegrationTest
     assert_select "header a[href=?]", new_writing_entry_path, "筆記開示を始める"
   end
 
+  test "redirects guests who try to create an entry" do
+    assert_no_difference("WritingEntry.count") do
+      post writing_entries_url, params: {
+        writing_entry: valid_writing_entry_params
+      }
+    end
+
+    assert_redirected_to login_url
+  end
+
+  test "creates a completed entry for the current user" do
+    login_as(@user)
+
+    assert_difference("@user.writing_entries.count", 1) do
+      post writing_entries_url, params: {
+        writing_entry: valid_writing_entry_params.merge(
+          status: "completed",
+          user_id: User.create!(
+            name: "別ユーザー",
+            email: "another-writing-user@example.com",
+            password: "password",
+            password_confirmation: "password"
+          ).id
+        )
+      }
+    end
+
+    writing_entry = @user.writing_entries.order(:created_at).last
+
+    assert_predicate writing_entry, :completed?
+    assert_equal 5, writing_entry.before_happiness_score
+    assert_equal 7, writing_entry.after_happiness_score
+    assert_equal "今日あったこと", writing_entry.event_detail
+    assert_equal "不安を感じた", writing_entry.negative_emotion_detail
+    assert_equal "うれしかった", writing_entry.positive_emotion_detail
+    assert_equal "まだ許せないこと", writing_entry.unforgiven_target_detail
+    assert_equal "穏やかに過ごしたい", writing_entry.tomorrow_hope
+    assert_redirected_to root_url
+    assert_equal "投稿を保存しました", flash[:notice]
+  end
+
+  test "creates a draft entry for the current user" do
+    login_as(@user)
+
+    assert_difference("@user.writing_entries.count", 1) do
+      post writing_entries_url, params: {
+        writing_entry: valid_writing_entry_params.merge(status: "draft")
+      }
+    end
+
+    assert_predicate @user.writing_entries.order(:created_at).last, :draft?
+    assert_redirected_to root_url
+    assert_equal "下書きを保存しました", flash[:notice]
+  end
+
+  test "renders the form with errors when the entry is invalid" do
+    login_as(@user)
+
+    assert_no_difference("WritingEntry.count") do
+      post writing_entries_url, params: {
+        writing_entry: valid_writing_entry_params.merge(
+          event_detail: "あ" * 3001,
+          status: "completed"
+        )
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "[role=alert]", text: /入力内容を確認してください/
+    assert_select "[role=alert]", text: /実際に起きたことは3000文字以内で入力してください/
+    assert_select "textarea[name=?]", "writing_entry[event_detail]", text: "あ" * 3001
+  end
+
   private
 
   def login_as(user)
@@ -68,6 +142,18 @@ class WritingEntriesControllerTest < ActionDispatch::IntegrationTest
         email: user.email,
         password: "password"
       }
+    }
+  end
+
+  def valid_writing_entry_params
+    {
+      before_happiness_score: 5,
+      after_happiness_score: 7,
+      event_detail: "今日あったこと",
+      negative_emotion_detail: "不安を感じた",
+      positive_emotion_detail: "うれしかった",
+      unforgiven_target_detail: "まだ許せないこと",
+      tomorrow_hope: "穏やかに過ごしたい"
     }
   end
 end
