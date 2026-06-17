e0365a721ded8662906db9b362c9a3ecbb42b3a5
Tomoyuki <tomoyuki@DESKTOP-V1L3SK5.localdomain>
2026-06-17 10:59:15 +0900

    プロフィール画面を実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,24 @@
 class ProfilesController < ApplicationController
   before_action :require_login
+  before_action :set_user
+
+  def show; end
+
+  def update
+    if @user.update(profile_params)
+      redirect_to profile_path, notice: t(".success")
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def profile_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
