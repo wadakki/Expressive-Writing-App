98fbdd2fd5b48b98916339dfd8e9eeeb33a8b976
Tomoyuki <tomoyuki@DESKTOP-V1L3SK5.localdomain>
2026-06-17 12:13:00 +0900

    issue#19終了。タイマーの細かな制御を実装しました。

--- app/javascript/controllers/writing_timer_controller.js
+++ app/javascript/controllers/writing_timer_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["display", "progress"]
+  static targets = ["container", "display", "progress", "timeoutMessage"]
   static values = {
+    alertMessage: String,
     duration: { type: Number, default: 480 }
   }
 
   connect() {
     this.remainingSeconds = this.durationValue
+    this.hasFinished = false
     this.render()
     this.interval = window.setInterval(() => {
       this.tick()
@@ -15,9 +17,7 @@ export default class extends Controller {
   }
 
   disconnect() {
-    if (this.interval) {
-      window.clearInterval(this.interval)
-    }
+    this.clearTimer()
   }
 
   tick() {
@@ -28,6 +28,10 @@ export default class extends Controller {
 
     this.remainingSeconds -= 1
     this.render()
+
+    if (this.remainingSeconds === 0) {
+      this.finish()
+    }
   }
 
   render() {
@@ -44,5 +48,36 @@ export default class extends Controller {
     }
 
     return (this.remainingSeconds / this.durationValue) * 100
+  }
+
+  finish() {
+    if (this.hasFinished) {
+      return
+    }
+
+    this.hasFinished = true
+    this.clearTimer()
+    this.showTimeoutMessage()
+    this.updateFinishedStyle()
+
+    if (this.hasAlertMessageValue) {
+      window.alert(this.alertMessageValue)
+    }
+  }
+
+  clearTimer() {
+    if (this.interval) {
+      window.clearInterval(this.interval)
+    }
+  }
+
+  showTimeoutMessage() {
+    this.timeoutMessageTarget.classList.remove("hidden")
+  }
+
+  updateFinishedStyle() {
+    this.containerTarget.classList.add("border-amber-300", "bg-amber-50/95")
+    this.progressTarget.classList.remove("bg-blue-600")
+    this.progressTarget.classList.add("bg-amber-500")
   }
 }
